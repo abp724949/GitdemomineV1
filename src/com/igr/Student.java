6f28bc11e480113ec66f9c1b9df7b23f8290e6cd
Arjun <arbpatil07@gmail.com>
2023-07-02 13:18:08 +0530

    First Commit....!

---
+++ src/com/igr/Student.java
@@ -0,0 +1,17 @@
+package com.igr;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+		
+		for(int i=0 ; i<=10; i++) {
+			System.out.println(i);
+			
+			if(i==4) {
+				break;
+			}
+		}
+	}
+
+}
